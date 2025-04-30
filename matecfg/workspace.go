549de9897524036bdc76368dcf5762249bd16554
yangyile <gogit@github.com/go-xlan/gogit>
2025-04-30 16:23:14 +0700

    使用简化的workcfg包且把包简化为matecfg包名

--- matecfg/workspace.go
+++ matecfg/workspace.go
@@ -1,31 +1,29 @@
-package mateconfig
+package matecfg
 
 import (
 	"encoding/json"
 	"os"
 
-	"github.com/go-mate/go-work/workconfig"
+	"github.com/go-mate/go-work/workcfg"
 	"github.com/yyle88/must"
 	"github.com/yyle88/rese"
 )
 
-type WorkspaceConfig struct {
+type Workspace struct {
 	WorkRoot string   `json:"workRoot"` // 工作区根目录
 	Projects []string `json:"projects"` // 该 Workspace 中的项目路径
 }
 
-func NewWorkspaceConfig(path string) *WorkspaceConfig {
+func NewWorkspace(path string) *Workspace {
 	file := rese.P1(os.Open(path))
-	defer func() {
-		must.Done(file.Close())
-	}()
-	res := &WorkspaceConfig{}
+	defer rese.F0(file.Close)
+	res := &Workspace{}
 	must.Done(json.NewDecoder(file).Decode(res))
 	return res
 }
 
-func (wc *WorkspaceConfig) GetWorkspace() *workconfig.Workspace {
-	return &workconfig.Workspace{
+func (wc *Workspace) GetWorkspace() *workcfg.Workspace {
+	return &workcfg.Workspace{
 		WorkRoot: wc.WorkRoot,
 		Projects: wc.Projects,
 	}
